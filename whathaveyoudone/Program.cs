a78793da7391621ac1d1bf49ac13965961fc2ca7
giova <giovannib776@gmail.com>
2024-10-08 16:54:34 -0400

    Add project files.

---
+++ whathaveyoudone/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("what have you done?");
+            Thread.Sleep(200); 
+        }
+    }
+}
